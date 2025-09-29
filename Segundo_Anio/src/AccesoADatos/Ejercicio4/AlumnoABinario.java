e6159aa8bab0645504f3bf7fb370f82820af16c9
alumno <alumno@DAM219>
2025-09-29 11:10:44 +0200

    29/09

---
+++ Segundo_Anio/src/AccesoADatos/Ejercicio4/AlumnoABinario.java
@@ -0,0 +1,15 @@
+package AccesoADatos.Ejercicio4;
+
+import java.util.Scanner;
+
+public class AlumnoABinario {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Dame el ");
+		
+	}
+}
