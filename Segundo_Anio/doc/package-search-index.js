1c5edab8b439da89bf7e257235e4a965a935be92
guill <guill@DESKTOP-FTINI27>
2025-10-09 19:43:11 +0200

    09

---
+++ Segundo_Anio/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"Segundo_Anio","l":"ProgramacionServiciosProcesos.Entrega1"}];updateSearchResults();
--- Segundo_Anio/src/ProgramacionServiciosProcesos/Entrega1/SumoNum.java
+++ Segundo_Anio/src/ProgramacionServiciosProcesos/Entrega1/SumoNum.java
@@ -2,7 +2,21 @@ package ProgramacionServiciosProcesos.Entrega1;
 
 import java.util.Scanner;
 
+/**
+ * Clase {@code SumoNum} Suma los dos numeros, los recibe desde
+ * {@code Llamador}, esta clase se ejecuta como proceso hijo
+ * 
+ * @author Guille
+ * @version 1.0
+ */
 public class SumoNum {
+
+	/**
+	 * Lee dos números enteros desde el objeto {@link Scanner} leyendo los
+	 * aprametros a traves del system.in ya que en la otra clase le he puesto un
+	 * print write y no se lo he mandado como parametros de process builder
+	 * 
+	 */
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 
@@ -11,9 +25,11 @@ public class SumoNum {
 			int num2 = entrada.nextInt();
 
 			System.out.println("El resultado final es : " + (num1 + num2));
-			entrada.close();
+
 		} catch (Exception e) {
 			System.err.println("Tienen que ser dos números enteros");
+		} finally {
+			entrada.close();
 		}
 	}
 
