ae3183e2e5c808c7d59424fb80b5dcdc1e9207a5
guill <guillermo.hualde@gmail.com>
2025-10-14 20:17:12 +0200

    14

--- Segundo_Anio/src/ProgramacionServiciosProcesos/Ejercicio11/LeoNumsdeFicheroBinario.java
+++ Segundo_Anio/src/ProgramacionServiciosProcesos/Ejercicio11/LeoNumsdeFicheroBinario.java
@@ -3,28 +3,53 @@ package ProgramacionServiciosProcesos.Ejercicio11;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LeoNumsdeFicheroBinario {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+
 		File f = new File("C:\\Users\\alumno\\git\\SegundoAnio2\\Segundo_Anio\\"
 				+ "src\\ProgramacionServiciosProcesos\\Ejercicio11\\numeros.dat");
-		
 		Scanner entrada = new Scanner(System.in);
-		
-		System.out.println("Dime por cual número quieres empezar :");
-		int numEmp = entrada.nextInt();
-		System.out.println("En que número quieres acabar");
-		int numAcab = entrada.nextInt();
-		
-		try(InputStream = new DataInputStream(new FileInputStream(f))){}
-		for(int i = numEmp; i <= numAcab; i++) 
-		{
-			
-			
+		int contadorSuma = 0;
+
+		try {
+			if (!f.exists()) {
+				throw new FileNotFoundException("Fichero no existe");
+			}
+
+			System.out.println("Dime por cual número quieres empezar :");
+			int numEmp = entrada.nextInt();
+			System.out.println("En que número quieres acabar");
+			int numAcab = entrada.nextInt();
+
+			try (DataInputStream entradaDatos = new DataInputStream(new FileInputStream(f))) {
+				if (numEmp < numAcab) {
+					List<Integer> numerosBinarios = new ArrayList<>();
+					while (entradaDatos.available() > 0) {
+						numerosBinarios.add(entradaDatos.readInt());
+					}
+
+					for (int i = numEmp - 1; i <= numAcab; i++) {
+						contadorSuma += numerosBinarios.get(i);
+
+					}
+				} else {
+					System.out.println("El primer número no puede ser mayor al segundo");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} finally {
+			System.out.println("El resultado es : " + contadorSuma);
+			entrada.close();
 		}
-		
+
 	}
 }
