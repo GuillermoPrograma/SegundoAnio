6429bb56fd861f93227d8141235542eafbd46049
alumno <guillermo.hualde@gmail.com>
2025-10-30 11:08:50 +0100

    Merge branch 'main' of https://github.com/GuillermoPrograma/SegundoAnio into main

--- Segundo_Anio/src/AccesoADatos/Ejercicio3/Ejercicio3_2/LectorDosArchivos.java
+++ Segundo_Anio/src/AccesoADatos/Ejercicio3/Ejercicio3_2/LectorDosArchivos.java
@@ -21,14 +21,26 @@ public class LectorDosArchivos {
 				BufferedReader br1 = new BufferedReader(pr);
 				FileReader pr2 = new FileReader(archivoLec2);
 				BufferedReader br2 = new BufferedReader(pr2);) {
-			String linea;
-			String linea2;
+			String linea = "";
+			String linea2 = "";
 
 			while ((linea = br1.readLine()) != null && (linea2 = br2.readLine()) != null) {
+
 				bw.println(linea);
 
 				bw.println(linea2);
 
+			}
+			if (linea == null) {
+				while ((linea2 = br2.readLine()) != null) {
+					bw.println(linea2);
+				}
+			}
+			if (linea2 == null) {
+				bw.println(linea);
+				while ((linea = br1.readLine()) != null) {
+					bw.println(linea);
+				}
 			}
 
 		} catch (IOException e) {
